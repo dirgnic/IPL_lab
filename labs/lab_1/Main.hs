3a5c101e790e7b1607e397eff08a7063a2b1ca19
Ingrid Corobana <63605350+dirgnic@users.noreply.github.com>
2024-10-30 20:34:20 +0100

    Update Main.hs
    added boolean operations

--- labs/lab_1/Main.hs
+++ labs/lab_1/Main.hs
@@ -12,6 +12,7 @@ import LLVM.AST.Global (
   Global(name, returnType, basicBlocks))
 import LLVM.AST.Type (
   i1)
+import LLVM.AST.IntegerPredicate (IntegerPredicate(EQ))
 
 import LLVM.Module (
   withModuleFromAST, writeObjectToFile, File(File))
@@ -19,7 +20,6 @@ import LLVM.Context (
   withContext)
 import LLVM.Target (
   withHostTargetMachineDefault)
-
 
 -- | A language of boolean expressions with xor.
 data Exp =
@@ -29,39 +29,42 @@ data Exp =
   Not Exp |
   Xor Exp Exp
 
-
 -- | Generate an LLVM constant from a boolean expression.
 codegen :: Exp -> LLVM.Constant
-codegen (Lit False) = error "TODO"
-codegen (And lhs rhs) = error "TODO"
-codegen e = error "TODO"
+codegen (Lit False) = LLVM.Int 1 0  -- i1 false (0)
+codegen (Lit True) = LLVM.Int 1 1   -- i1 true (1)
+codegen (And lhs rhs) =
+  LLVM.And (codegen' lhs) (codegen' rhs)  -- AND
+codegen (Or lhs rhs) =
+  LLVM.Or (codegen' lhs) (codegen' rhs)   -- OR
+codegen (Xor lhs rhs) =
+  LLVM.Xor (codegen' lhs) (codegen' rhs)  -- XOR
+codegen (Not e) =
+  LLVM.Xor (codegen' e) (LLVM.Int 1 1)    -- NOT (XOR with 1)
 
+-- | Helper function to wrap LLVM.Constant as Operand.
+codegen' :: Exp -> LLVM.Constant
+codegen' = codegen
 
 -- | An example boolean expression.
 myExpression :: Exp
 myExpression = Xor (Lit True) (And (Lit False) (Lit True))
 
-
 -- | The result of generating code from 'myExpression'.
 myConstant :: LLVM.Constant
 myConstant = codegen myExpression
-
 
 -- | An LLVM constant operand.
 myOperand :: Operand
 myOperand = ConstantOperand myConstant
 
-
-
 -- | An LLVM terminator that returns a constant operand.
 myTerminator :: Terminator
 myTerminator = Ret (Just myOperand) []
 
-
 -- | An LLVM basic block with name \"myEntrypoint\" and no intructions.
 myBasicBlock :: BasicBlock
 myBasicBlock = BasicBlock "myEntrypoint" [] (Do myTerminator)
-
 
 -- | An LLVM definition of a single function \"myMain\".
 myDefinition :: Definition
@@ -70,11 +73,9 @@ myDefinition = GlobalDefinition (functionDefaults {
   returnType = i1,
   basicBlocks = [myBasicBlock]})
 
-
 -- | An LLVM module with name \"myModule\" and a single definition.
 myModule :: Module
 myModule = Module "myModule" "myModule.be" Nothing Nothing [myDefinition]
-
 
 -- | Write out an object file \"myModule.o\" corresponding to the constant
 -- LLVM module 'myModule'.
